8d0a34bb76c4d0fb1942a50eaa4e775b728dcc11
al001 <al001japan@gmail.com>
2012-07-04 19:59:40 +0900

    rikunabi*.rb merge

--- rikunabi.rb
+++ rikunabi.rb
@@ -2,7 +2,7 @@
 require 'mechanize'
 require 'pp'
 
-num = 668
+num = 731
 per = 30
 
 page = num / per + 1
@@ -12,19 +12,22 @@ a = Mechanize.new
 companies = []
 (1..page).each do |i|
   p = a.get('http://job.rikunabi.com/2014/search/pre/internship/result/?modulecd=100&kdb=T&pn=' + i.to_s)
-  companies += p.search(".g_txt_M").map{|l| l.text }
+  companies += p.search(".g_txt_M")
 end
 
-companies.delete("")
-
-companies.map do |i|
-  i.sub!(/株式会社/, "")
-  i.sub!(/有限会社/, "")
-  i.gsub!(/【.*?】/, "")
-  i.gsub!(/\(.*?\)/, "")
-  i.gsub!(/（.*?）/, "")
-  i.gsub!(/※.*/, "")
-  i.delete!(" 　")
+companies.delete_if do |company|
+  true if company.text.empty?
 end
 
-print companies.join("\n")
+companies.each do |company|
+  text = company.text
+  text.gsub!(/株式会社/, "")
+  text.gsub!(/有限会社/, "")
+  text.gsub!(/【.*?】/, "")
+  text.gsub!(/\(.*?\)/, "")
+  text.gsub!(/（.*?）/, "")
+  text.gsub!(/[.*?]/, "")
+  text.gsub!(/※.*/, "")
+  text.delete!(" 　")
+  puts "- [#{text}](http://job.rikunabi.com/#{company[:href]})"
+end
